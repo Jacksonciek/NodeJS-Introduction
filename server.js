0c5cd0711fa388d97c0d6a66cda4eb952c765c32
Jackson Lawrence <jacksonciek@gmail.com>
2025-03-10 16:58:07 +0700

    async await

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 import http from "http";
 // const PORT = 8000;
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
 const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__filename, __dirname);
+
+const server = http.createServer(async(req, res) => {
     // res.write("Hello World");
     // res.setHeader("Content-Type", "text/html");
     // res.statusCode = 404;
@@ -13,17 +21,40 @@ const server = http.createServer((req, res) => {
     // res.end("<h2>Hello World</h2>");
     // res.end(JSON.stringify({message:"Server Error"}));
     // res.end("<h1>Hello World</h1>");
-    if (req.url === "/"){
-        res.writeHead(200, {"Content-Type":"text/html"})
-        res.end("<h1>Homepage</h1>");
-    }
-    else if (req.url === "/about"){
-        res.writeHead(200, {"Content-Type":"text/html"})
-        res.end("<h1>About</h1>");
-    }
-    else{
-        res.writeHead(500, {"Content-Type":"text/html"})
-        res.end("<h1>Not Found</h1>");
+    try {
+        if (req.method === 'GET') {
+            // if (req.url === "/") {
+            //     res.writeHead(200, { "Content-Type": "text/html" });
+            //     res.end("<h1>Homepage</h1>");
+            // }
+            // else if (req.url === "/about") {
+            //     res.writeHead(200, { "Content-Type": "text/html" });
+            //     res.end("<h1>About</h1>");
+            // }
+            // else {
+            //     res.writeHead(500, { "Content-Type": "text/html" });
+            //     res.end("<h1>Not Found</h1>");
+            // }
+            let filePath;
+            if (req.url === "/") {
+                filePath = path.join(__dirname, 'public', 'index.html');
+            }
+            else if (req.url === "/about") {
+                filePath = path.join(__dirname, 'public', 'about.html');
+            }
+            else {
+                throw new Error('Not Found');
+            }
+            const data = await fs.readFile(filePath);
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            res.end();
+        } else {
+            throw new Error('Method not allowed');
+        }
+    } catch (error) {
+        res.writeHead(500, {"Content-Type":"text/plain"});
+        res.end("Server Error");
     }
 });
 
