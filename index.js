f1872758fdedcbf928d21b52fe247232851ef43c
Jackson Lawrence <jacksonciek@gmail.com>
2025-03-10 14:55:31 +0700

    Post Controller

---
+++ index.js
@@ -0,0 +1,10 @@
+import getPosts, {getPostsLength} from "./postController.js";
+
+console.log(getPosts());
+
+console.log(`Posts Length : ${getPostsLength()}`);
+
+// const {generateRandomNumbers, celciusToFahrenheit} = require('./utils');
+
+// console.log(`Random Number : ${generateRandomNumbers()}`)
+// console.log(`Celcius to Fahrenheit : ${celciusToFahrenheit(10)}`)
