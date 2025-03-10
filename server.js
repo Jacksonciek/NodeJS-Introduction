8fef525d0d06079570a70c9086e8584ea368bc47
Jackson Lawrence <jacksonciek@gmail.com>
2025-03-10 16:25:39 +0700

    POSTMAN

--- server.js
+++ server.js
@@ -6,13 +6,25 @@ const server = http.createServer((req, res) => {
     // res.write("Hello World");
     // res.setHeader("Content-Type", "text/html");
     // res.statusCode = 404;
-    console.log(req.url);
-    console.log(req.method);
+    // console.log(req.url);
+    // console.log(req.method);
     // res.writeHead(200, {"Content-Type":"application/json"})
-    res.writeHead(200, {"Content-Type":"text/html"})
+    // res.writeHead(200, {"Content-Type":"text/html"})
     // res.end("<h2>Hello World</h2>");
     // res.end(JSON.stringify({message:"Server Error"}));
-    res.end("<h1>Hello World</h1>");
+    // res.end("<h1>Hello World</h1>");
+    if (req.url === "/"){
+        res.writeHead(200, {"Content-Type":"text/html"})
+        res.end("<h1>Homepage</h1>");
+    }
+    else if (req.url === "/about"){
+        res.writeHead(200, {"Content-Type":"text/html"})
+        res.end("<h1>About</h1>");
+    }
+    else{
+        res.writeHead(500, {"Content-Type":"text/html"})
+        res.end("<h1>Not Found</h1>");
+    }
 });
 
 server.listen(PORT, () => {
